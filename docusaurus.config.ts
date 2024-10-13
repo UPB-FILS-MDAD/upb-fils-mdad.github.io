0601768f499d34fd5f532d8124e9c47ed46a0dba
Amalia Simion <amalia.simion@stud.fils.upb.ro>
2024-10-13 20:59:00 +0300

    fix edit link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/UPB-FILS-MDAD/upb-fils-mdad.github.io/edit",
+            "https://github.com/UPB-FILS-MDAD/upb-fils-mdad.github.io/edit/main",
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
